c8e42db43619cafbe1f3d60ebdf14a2e75f79817
dontloseyourheadsu <dontloseyourheadsu@gmail.com>
2025-07-15 12:56:13 -0600

    Removed routes and ssr

--- frontend/wiccapedia-frontend/src/app/app.config.ts
+++ frontend/wiccapedia-frontend/src/app/app.config.ts
@@ -1,16 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withFetch()),
-    provideClientHydration(withEventReplay())
+    provideHttpClient(withFetch())
   ]
 };
