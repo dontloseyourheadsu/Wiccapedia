c3dd3fc99a5c6ba20bf9cb0de4aefa5a2eb20120
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-11-08 21:31:20 -0600

    dotnet aspnet mvc project

---
+++ WiccapediaGrimoire/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WiccapediaGrimoire.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
