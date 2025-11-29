6709efa9c9774babe03cb010d25dfa0c6c1c6a7c
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-11-29 16:45:36 -0600

    Remove MVC project to Blazor WASM SPA

--- WiccapediaGrimoire/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WiccapediaGrimoire.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
