cfad041b71c1a34f6e1447f4c3b9b23fe2685583
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2025-12-06 19:05:27 -0600

    Wiccapedia app use custom authhandler + http client factory

--- WiccapediaGrimoire/Program.cs
+++ WiccapediaGrimoire/Program.cs
@@ -2,18 +2,27 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WiccapediaGrimoire;
 using WiccapediaGrimoire.Services.Covers;
+using WiccapediaGrimoire.Utils.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<CoversService>();
 
+// 1. Register the Handler
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+
+// 2. Register the Service AND the Client together
+builder.Services.AddHttpClient<CoversService>(client =>
+    {
+        client.BaseAddress = new Uri(builder.Configuration["WiccapediaApi:BaseUrl"]!);
+    })
+    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+
+// 3. Auth Configuration
 builder.Services.AddOidcAuthentication(options =>
 {
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
 });
 
